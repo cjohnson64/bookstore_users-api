bac69f5f977de667de4db523ab5a5466111730af
crjohnson1208 <collin.johnson@callibrity.com>
2019-12-23 12:23:52 -0500

    unit testing implemented

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,9 +3,25 @@ package users
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/crjohnson1208/bookstore_users-api/domain/users"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
 )
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		//TODO: Handle error
+		return	
+	}
+	if err := json.Unmarshall(bytes, &user); err!= nil{
+		//TODO: Handle Json Error
+		return
+	}
+	fmt.Println(user)
 	c.String(http.StatusNotImplemented, "implement me!")
 }
 
---
+++ controllers/users/users_controller_test.go
@@ -0,0 +1,41 @@
+package users
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestCreateUser(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			CreateUser(tt.args.c)
+		})
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetUser(tt.args.c)
+		})
+	}
+}
