72b383ea947e5600605f0730ea5cbba9296d2e17
crjohnson1208 <collin.johnson@callibrity.com>
2019-12-31 13:07:18 -0500

    api working with mock db and postman request

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,34 +4,44 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"github.com/crjohnson1208/bookstore_users-api/domain/users"
-	"io/ioutil"
-	"fmt"
-	"encoding/json"
+	// "io/ioutil"
+	// "fmt"
+	// "encoding/json"
 	"github.com/crjohnson1208/bookstore_users-api/services"
+	"github.com/crjohnson1208/bookstore_users-api/utils/errors"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		//TODO: return bad request to the caller
-		return	
-	}
-	if err := json.Unmarshal(bytes, &user); err!= nil{
-		//TODO: handle user creation error
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
-	result, saveErr := services.CreateUser()
 
-
-	fmt.Println(user)
-	c.String(http.StatusNotImplemented, "implement me!")
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user ID should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
 	
-	c.String(http.StatusNotImplemented, "implement me!")
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusCreated, user)
 }
 
 func SearchUser(c *gin.Context) {
---
+++ domain/users/user_dao.go
@@ -0,0 +1,39 @@
+package users 
+
+import (
+	"github.com/crjohnson1208/bookstore_users-api/utils/errors"
+	"fmt"
+)
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+func (user *User) Get() *errors.RestErr{
+	result := usersDB[user.Id]
+	if result == nil {
+		return errors.NewBadRequestError(fmt.Sprintf("user %d already", user.Id))
+	}
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+
+}
+
+
+func (user *User) Save() *errors.RestErr {
+	current := usersDB[user.Id]
+	if current != nil {
+		if current.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Id))
+		} 
+		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	}
+	usersDB[user.Id] = user
+	return nil
+
+}
