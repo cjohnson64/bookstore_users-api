ea9fada7e17028e6804b892c84218fed91b9a557
crjohnson1208 <collin.johnson@callibrity.com>
2020-01-03 13:25:38 -0500

    user creation working with mysql db and error handling

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,7 +2,14 @@ package users
 
 import (
 	"github.com/crjohnson1208/bookstore_users-api/utils/errors"
+	"github.com/crjohnson1208/bookstore_users-api/utils/date_utils"
 	"fmt"
+	"github.com/crjohnson1208/bookstore_users-api/datasources/mysql/users_db"
+	"strings"
+)
+
+const(
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 var (
@@ -10,6 +17,9 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr{
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already", user.Id))
@@ -26,14 +36,30 @@ func (user *User) Get() *errors.RestErr{
 
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Id))
-		} 
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	usersDB[user.Id] = user
-	return nil
+	defer stmt.Close()
+	
+	user.DateCreated = date_utils.GetNowString()
 
+	insertResult, err :=  stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), "email_UNIQUE"){
+			return errors.NewBadRequestError(
+				fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to add user: %s", err.Error()))	
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err !=nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s ", err.Error()))
+	}
+
+	user.Id = userId
+	return nil
 }
